8ec69876883d40f86be3f13f550bebfcf9785c15
zhao-qian6 <2827871771@qq.com>
2023-11-03 10:38:08 +0800

    11.3

---
+++ 10.24 .c/源.c
@@ -0,0 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//   
+//        /*int a = 0 , b=0, c=0;
+//        int second = 0;
+//        int d = 0;
+//        scanf("%d", &second);
+//        
+//            a = second / 3600;
+//            d = second % 3600;
+//            b = d / 60;
+//            c = d % 60;
+//            printf("%d %d %d", a, b, c);*/
+//    /*double a = 0.0, b = 0.0, c = 0.0;
+//    double sum = 0.0, ave = 0.0;
+//    scanf("%lf %lf %lf", &a, &b, &c);
+//    sum = a + b + c;
+//    ave = (sum) / 3;
+//    printf("%.2lf %.2lf", sum, ave);
+//    return 0;*/
+//
+//    //long long w = 0, h = 0;
+//    //double BMI = 0.0;
+//    //scanf("%ld %ld", &w, &h);
+//    //
+//    //printf("%.2lf",(double) w/(h*h));
+//
+//    //return 0; 
+//    //int w = 0, h = 0;
+//    //double BMI = 0.0;
+//    //scanf("%d %d", &w, &h);
+//    //double heigh = 0;
+//    //heigh = h / 100.0;
+//    //BMI = w / (heigh * heigh);
+//    //printf("%.2lf", BMI);//注意h单位是厘米，要化成米
+//    /*int a = 0, b = 0, c = 0;
+//    double circumference = 0, area = 0;
+//    
+//    scanf("%d %d %d", &a, &b, &c);
+//    circumference = (double)a + b + c;
+//    area = (double)a * b * c;//sqrt(p*(p-a)*(p-b)*(p-c))三角形面积公式，squrt表示开平方
+//    printf("%.2lf %.2lf", circumference, area);
+//    return 0;*/
+//    /*double r = 0.0, V = 0.0;
+//    double π = 3.1415926;
+//    scanf("%lf", &r);
+//
+//    V = 4 * π * (r * r * r) / 3;
+//    printf("%.3lf", V);
+//    return 0;*/
+//    int n = 0;;
+//    ////移位运算，不会
+//    //while (scanf("%d", &n) != EOF)
+//    //{
+//    //    printf("%d", 2 << (n - 1));
+//    //}
+//    //return 0;
+//        
+//        return 0;
+//  
+//}
+int main()
+{
+   /* int m = 0, n = 0, h = 0;
+    int a = 0;
+    scanf("%d %d %d", &n, &h, &m);
+
+    a = m / h;
+    if (m % h == 0)
+        printf("%d", n - a);
+    else
+        printf("%d", n - 1 - a);
+
+    return 0;*/
+    /*printf("I lost my cellphone!");
+    return 0;*/
+    /*int a = 0, b = 0, c = 0;
+    printf("Name    Age    Gender\n");
+    printf("---------------------\n");
+    printf("Jack    18     man\n");
+
+    return 0;*/
+    /*int a = 0, b = 0, c = 0, d = 0, e = 0;
+    double ave = 0.0;
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    ave = (double)(a + b + c + d + e) / 5;
+    printf("%.1lf", ave);*/
+
+
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
