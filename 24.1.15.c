1f22fe0fbdd62d3ed459f01e6d7542a213363cc4
zhaojiu34 <149633557+zhaojiu34@users.noreply.github.com>
2024-01-15 22:39:23 +0800

    Add files via upload

---
+++ 24.1.15.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//#define OFFSETOF(type, member) ((size_t)&((type*)0)->member)
+//struct Stu
+//{
+//	int age;
+//	char name[20];
+//	double score;
+//	double heigh;
+//};
+//int main()
+//{
+//	printf("%zd\n", OFFSETOF(struct Stu, age));
+//	printf("%zd\n", OFFSETOF(struct Stu, name));
+//	printf("%zd\n", OFFSETOF(struct Stu, score));
+//	printf("%zd\n", OFFSETOF(struct Stu, heigh));
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//#define SWAP_BIT(x) (x = (((x & 0x55555555)<<1)+ ((x & 0xaaaaaaaa)>>1)))
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	SWAP_BIT(n);
+//	printf("%d", n);
+//	return 0;
+//}
+
